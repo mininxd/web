026c2029440079e53a49a51dc0f9ab62379edf5a
mininxd <mininsna@gmail.com>
2024-11-12 15:48:54 +0000

    Memperbaiki UI dan menambahkan tombol download

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-fetch("https://api.mininxd.my.id/brat/limit").then(res => {
- return res.json() 
-}).then(data => {
-  console.log(data)
-  })
+// reset filename each enter websites
+if(localStorage.getItem("filename")) {
+  localStorage.removeItem("filename");
+}
 
 
 
@@ -11,23 +10,36 @@ generateBtn.addEventListener('click', () => {
       msg.innerHTML = "Masukan teks dulu";
       msgBar.style.display = "block";
     } else {
+      bratImg.style.opacity = 1;
       generateBtn.setAttribute('disabled', true);
       bratImg.src = "./assets/Program_wait.ico";
       fetch(`https://api.mininxd.my.id/brat?txt=${inputText.value}`)
         .then(res => {
          return res.json() 
       }).then(data => {
+        if(!data.url) {
+         bratImg.style.opacity = 0;
+         msg.innerHTML = "<strong style='color:red'>Error, coba lagi</strong>"
+        } else {
+         localStorage.setItem("filename", data.path);
          bratImg.src = data.url;
-         msg.innerHTML = `<marquee>Cara simpan gambar cukup di hold lalu <strong>Simpan Gambar</strong>, &nbsp; untuk desktop klik kanan lalu <strong>Save As</strong></marquee>`
+          bratImg.addEventListener('load', () => {
+            downBtn.style.display = "block";
+            imgWrapper.style.background = "#fff";
+            msg.innerHTML = `<marquee>Cara simpan gambar cukup di hold lalu <strong>Simpan Gambar</strong>, &nbsp; untuk desktop klik kanan lalu <strong>Save As</strong>, &nbsp; atau pencet tombol <strong>Download</strong></marquee>`
+          })
+        }
       }).finally(() => {
-         msgBar.style.display = "block";
-         generateBtn.removeAttribute('disabled')
-    })
+        bratImg.addEventListener('load', () => {
+           msgBar.style.display = "block";
+           generateBtn.removeAttribute('disabled')
+        })
+      })
   }
 })
 
 inputText.addEventListener('focus', () => {
-  bratImg.style.height = "10vh"
+  bratImg.style.height = "8vh"
 })
 inputText.addEventListener('blur', () => {
   bratImg.style.height = "auto"
