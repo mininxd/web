fc9e9a149f43225ee2163f39bb7989f12628ce03
mininxd <mininsna@gmail.com>
2024-12-04 04:01:01 +0000

    Masih Work in Progress

---
+++ js/deleteList.js
@@ -0,0 +1,1 @@
+itemListToDelete.innerHTML += ``
--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
-import {qris} from "../lib/generate.js";
+import {qris} from "../lib/qris.js";
+import "/lib/modal.js";
 import "./login.js";
-import "./modal.js";
+import "./localStorage.js";
+import "./canvas.js";
 import QRCode from "qrcode";
 
 
+// Get Header
+downloadAll.disabled = true;
+downloadAll.classList.add("is-loading");
+qris(localStorage.getItem("QRIS_Utama"), 0)
+.then(data => {
+downloadAll.disabled = false;
+downloadAll.classList.remove("is-loading");
 
+dataQris.innerHTML = data.QR;
+QRCode.toCanvas(merchantQRIScanvas, data.QR);
+namaMerchant.innerHTML = data.merchant;
+namaMerchant.classList.remove("is-skeleton");
 
-// Test
-(async() => {
-if(localStorage.getItem("QRIS_Utama")) {
-  let qrisData = await qris(localStorage.getItem("QRIS_Utama"), 1000)
-  
-QRCode.toCanvas(qrTest, qrisData);
-QRCode.toCanvas(merchantQRIScanvas, localStorage.getItem("QRIS_Utama"));
-}
-})()
+downloadAll.addEventListener("click", () => {
+  downloadAll.classList.add("is-loading");
+  try {
+      htmlToImage.toPng(listQrisCanvas).then(function (blob) {
+        if (window.saveAs) {
+          window.saveAs(blob, `${data.merchant}.png`);
+        } else {
+          FileSaver.saveAs(blob, `${data.merchant}.png`);
+        }
+      });
+    setTimeout(() => {
+  downloadAll.classList.remove("is-loading");
+    },500)
+    } catch (e) {
+      alert(e);
+    }
+})
+
+}).catch(e => {
+  alert(e)
+})
+
