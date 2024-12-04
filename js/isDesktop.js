3fb5f7eb9cb8fb15fd8a97b29ac10ce6a9977800
mininxd <mininsna@gmail.com>
2024-12-04 09:33:07 +0000

    Test

--- js/isDesktop.js
+++ js/isDesktop.js
@@ -2,9 +2,13 @@ if(!navigator.userAgent.includes("Mobile")) {
   gridCanvas.classList.remove("has-3-cols");
   gridCanvas.classList.add("has-5-cols");
   listQrisCanvas.style.padding = "1em 8vw 1em 8vw";
+  navbar.style.paddingLeft = "2%"
   const buttons = document.querySelectorAll('#canvasBtn .button');
   buttons.forEach(button => {
-    button.style.width = "75%"
+    button.style.width = "50%"
   });
+  document.querySelectorAll('.modal-content').forEach(div => {
+   div.style.width = "40%"
+});
 
 }
