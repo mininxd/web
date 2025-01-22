9864960c1da984fab43b37f9985b0a4e61c474b3
mininxd <mininsna@gmail.com>
2025-01-22 08:51:07 +0700

    add more refer regex

--- src/referrer.js
+++ src/referrer.js
@@ -5,7 +5,7 @@ if (referrer) {
   if (referrer.match(/ads|track/)) {
     judul.innerHTML = "Situs Tracker"
     informasi.innerHTML = "Anda tampaknya mencoba mengakses situs pelacak. Demi keamanan dan privasi Anda, akses ke situs ini telah dihentikan."
-  } else if(referrer.match(/gamble|gambling|bet|cuan|zeus|slot/)) {
+  } else if(referrer.match(/gamble|raja|toto|abadi|octa|casino|fx|naga|kiu|gambling|bet|cuan|zeus|slot/)) {
     judul.innerHTML = "Situs Gambling"
     informasi.innerHTML = "Situs ini dideteksi sebagai platform perjudian. Aktivitas semacam ini berisiko terhadap keamanan finansial dan privasi Anda. Akses telah dihentikan"
   }
