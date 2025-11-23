ffe1d9895c02034edce4acba605f5a240702167c
mininxd <mininsna@gmail.com>
2025-11-23 09:00:05 +0000

    dns

--- src/blocked.ts
+++ src/blocked.ts
@@ -1,8 +1,6 @@
 if(!window.location.origin.includes("dns.mininxd.xyz")) {
   console.log(true)
-/*
-blocked_content.classList.remove("hidden");
   blocked_content.classList.remove("hidden");
+  blocked_content.style.display("block");
   dns_content.classList.add("hidden");
-  */
 }
