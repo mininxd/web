7d32f7b39f9761a22ac25ce2b30c898f9fa48a22
mininxd <mininsna@gmail.com>
2025-11-23 09:05:45 +0000

    dns

--- src/blocked.ts
+++ src/blocked.ts
@@ -4,4 +4,25 @@ if(!window.location.origin.includes("dns.mininxd.xyz")) {
   blocked_content.style.display = "block";
   dns_content.classList.add("hidden");
   dns_content.style.display = "none";
+  
+  blockedUrl.append(window.location.origin);
+
+let i = 5;
+function countdown() {
+  i--;
+  blocked_redirect.innerHTML = i;
+
+  if (i <= 0) {
+    if (document.referrer) {
+      window.location.href = document.referrer;
+    } else {
+      window.history.back();
+    }
+    return;
+  }
+
+  setTimeout(countdown, 1000);
+}
+
+countdown();
 }
