c4167c50a3815087afdd5136ccdfd70e1acddade
mininxd <mininsna@gmail.com>
2025-11-23 09:01:08 +0000

    dns

--- src/blocked.ts
+++ src/blocked.ts
@@ -1,6 +1,7 @@
 if(!window.location.origin.includes("dns.mininxd.xyz")) {
   console.log(true)
   blocked_content.classList.remove("hidden");
-  blocked_content.style.display("block");
+  blocked_content.style.display = "block";
   dns_content.classList.add("hidden");
+  dns_content.style.display = "none";
 }
